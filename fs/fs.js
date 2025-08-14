9ce709b542151cd8ec7eeaade006e3e0ebc98e38
Midhun Ambadan <midhunambadan06@gmail.com>
2025-08-14 13:16:28 +0530

    fs added

--- fs/fs.js
+++ fs/fs.js
@@ -1,2 +1,15 @@
 
 const fs=require('fs')
+
+const path=require('path')
+const file=path.join(__dirname,'sample.txt')
+
+fs.writeFile(file,'Hello bro',(err)=>{
+    if(err){
+        console.log(err)
+    }
+
+    console.log('file created')
+})
+
+
